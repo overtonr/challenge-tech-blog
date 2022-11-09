0ea05d0f8bc3e926ed0a1deae5592963e677a628
Rebecca Overton <rebeccaoverton@Rebeccas-MacBook-Air.local>
2022-11-08 22:55:06 -0800

    minor additions to userRoutes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const { User } = require('../../models');
-const { use } = require('./blogRoutes');
 
+
+//POST route starts session
 router.post('/', async(req, res) => {
     try {
         const userData = await User.create(req.body)
@@ -15,6 +16,8 @@ router.post('/', async(req, res) => {
     }
 });
 
+
+//user validation for logging in with correct credentials
 router.post('/login', async (req,res) => {
     try{
         const userData = await User.findOne({ where: {email: req.body.email}});
@@ -36,3 +39,17 @@ router.post('/login', async (req,res) => {
         res.status(400).json(err);
     }
 });
+
+
+//logout request
+router.post('/logout', (req, res) =>{
+    if(req.session.logged_in) {
+        req.session.destroy(() =>{
+            res.status(204).end();
+        })
+    } else{
+        res.status(404).end();
+    }
+});
+
+module.exports = router;
